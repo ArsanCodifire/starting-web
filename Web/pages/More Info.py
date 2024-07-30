26c47580dfe96596d40ece1faa7e965fa4062ef2
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-30 18:05:01 +0530

    Update More Info.py

--- Web/pages/More Info.py
+++ Web/pages/More Info.py
@@ -3,7 +3,7 @@ st.title("Website created by Arush Krishna A.S.")
 st.title("My website")
 st.write("This is the power of python")
 yn=st.selectbox("Do you know python ?",["Yes","No"])
-if yn==Yes:
+if yn=="Yes":
     st.write("Good,your the best!!")
 else:
     st.write("No Problem.")
