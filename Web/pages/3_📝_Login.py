d7646b81af81d259532fb5e3778d0375e5f58412
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-30 22:44:35 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -1,21 +1,21 @@
 import streamlit as st
-import pyrebase as fb
-firebaseConfig = {
-  "apiKey": "AIzaSyDWruUPKro1RzE5ZejZ2G7ie9Pm22hz814",
-  "authDomain": "py-login-55126.firebaseapp.com",
-  "databaseURL":"https://console.firebase.google.com/project/py-login-55126/database/py-login-55126-default-rtdb/data/~2F",
-  "projectId": "py-login-55126",
-  "storageBucket": "py-login-55126.appspot.com",
-  "messagingSenderId": "378309201398",
-  "appId": "1:378309201398:web:bda344222cbea996030bd7",
-  "measurementId": "G-3B7H2D1QD5"
-}
+import pyrebase 
+firebaseConfig = {'apiKey': "AIzaSyDm2HeGl3bApix5KsbhI8NOjdwXkhNTaJM",
+                  'authDomain': "trialauth-7eea1.firebaseapp.com",
+                  'databaseURL': "https://trialauth-7eea1.firebaseio.com",
+                  'projectId': "trialauth-7eea1",
+                  'storageBucket': "trialauth-7eea1.appspot.com",
+                  'messagingSenderId': "441088628124",
+                  'appId': "1:441088628124:web:59f51ba5b6a475032f2459",
+                  'measurementId': "G-TNR2V3DEQD"}
 
-fb.initialize_app(firebaseConfig)
+fb=pyrebase.initialize_app(firebaseConfig)
 auth=fb.auth()
 def signin():
   ul=auth.create_user_with_email_and_password(email,psd)
-  st.title(f"Welcome{usr}")
+  if ul:
+    st.title(f"Welcome{usr}")
+#comment
 lg=st.form("Sign in")
 usr=lg.text_input("Username")
 email=lg.text_input("Email")
