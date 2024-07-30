dbb176c17a3da53f42ffb2274073a32a1f766637
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-30 16:34:26 +0530

    Update Grammer.py

--- Web/pages/Grammer.py
+++ Web/pages/Grammer.py
@@ -1,10 +1,10 @@
 import streamlit as st
-import language_tool_python 
+from gingerit.gingerit import GingerIt
 st.title("Grammer Checker")
 txt=st.text_area("Type here")
 
 def check_grammar(text):
-    parser = language_tool_python.LanguageToolPublicAPI('en-US')
+    parser = GingerIt() 
     result = parser.check(text)
     ans = parser.correct(text)
     return ans
