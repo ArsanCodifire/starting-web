99d9d225301cebeaf916cc1604540e0d1bd4de09
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-30 22:46:00 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -1,20 +1,8 @@
 import streamlit as st
-import pyrebase 
-firebaseConfig = {'apiKey': "AIzaSyDm2HeGl3bApix5KsbhI8NOjdwXkhNTaJM",
-                  'authDomain': "trialauth-7eea1.firebaseapp.com",
-                  'databaseURL': "https://trialauth-7eea1.firebaseio.com",
-                  'projectId': "trialauth-7eea1",
-                  'storageBucket': "trialauth-7eea1.appspot.com",
-                  'messagingSenderId': "441088628124",
-                  'appId': "1:441088628124:web:59f51ba5b6a475032f2459",
-                  'measurementId': "G-TNR2V3DEQD"}
 
-fb=pyrebase.initialize_app(firebaseConfig)
-auth=fb.auth()
+
 def signin():
-  ul=auth.create_user_with_email_and_password(email,psd)
-  if ul:
-    st.title(f"Welcome{usr}")
+  st.title(f"Welcome{usr}")
 #comment
 lg=st.form("Sign in")
 usr=lg.text_input("Username")
