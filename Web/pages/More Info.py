aea9de21c3177dcf3a91456abba41307316c4fdd
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-30 17:56:50 +0530

    Update More Info.py

--- Web/pages/More Info.py
+++ Web/pages/More Info.py
@@ -2,4 +2,9 @@ import streamlit as st
 st.title("Website created by Arush Krishna A.S.")
 st.title("My website")
 st.write("This is the power of python")
-st.toggle("Yes/No")
+yn=st.selectbox("Do you know python ?",["Yes","No"])
+if yn=Yes:
+    st.write("Good,your the best!!")
+else:
+    st.write("No Problem.")
+  
