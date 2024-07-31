8384f600ef8b6799d0ed680a1940c0808b802c5b
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-31 18:42:40 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -6,7 +6,7 @@ def signin():
 #comment
 st.selectbox("Choose",("NA","Sign In","Login"))
 if login=="Login":
-    lg=st.form("Sign in"
+    lg=st.form("Sign in")
     email=lg.text_input("Email")
     psd=lg.text_input("Password",type="password")
     btn=lg.form_submit_button("Sign in",help=None,on_click=signin)
