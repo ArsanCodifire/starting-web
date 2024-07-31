8be6b3d7c10d1008ed7fb01391f9788ecd0bbbbd
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-07-31 18:42:12 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -4,15 +4,17 @@ import streamlit as st
 def signin():
   st.write(f"Welcome {usr}")
 #comment
-st.selectbox("",)
-if login="Login":
-    lg=st.form("Sign in")
+st.selectbox("Choose",("NA","Sign In","Login"))
+if login=="Login":
+    lg=st.form("Sign in"
     email=lg.text_input("Email")
     psd=lg.text_input("Password",type="password")
     btn=lg.form_submit_button("Sign in",help=None,on_click=signin)
-else:
+elif login=="Sign In":
     lg=st.form("Sign in")
     usr=lg.text_input("Username")
     email=lg.text_input("Email")
     psd=lg.text_input("Password",type="password")
     btn=lg.form_submit_button("Sign in",help=None,on_click=signin)
+else:
+    return usr
