e344543f5c453af9425ef5d03e95bcfc180dff6c
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-01 19:59:56 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -1,14 +1,25 @@
 import streamlit as st
-
+from deta import Deta
+api="EyDDRDhg_iyiNV4ShnC617vy7Zww4iEJxFbTKUVzz"
+dt=Deta(api)
+db=dt.Base("Loginid")
 
 def signin():
-  st.write(f"Welcome {usr}")
+    db.put({"key":email,"username":usr,"password":psd})
+    st.write(f"Hi{usr}")
+def login():
+    log_e=dB.fetch()["key"]
+    log_p=dB.fetch()["password"]
+    log_u=dB.fetch()["username"]
+    if emaill==log_e:
+        if psdl==log_p:
+            st.write(f"Welcome back{log_u}")
 #comment
 login=st.selectbox("Choose",options=("NA","Sign In","Login"))
 if login=="Login":
     lg=st.form("Sign in")
-    email=lg.text_input("Email")
-    psd=lg.text_input("Password",type="password")
+    emaill=lg.text_input("Email")
+    psdl=lg.text_input("Password",type="password")
     btn=lg.form_submit_button("Sign in",help=None,on_click=signin)
 elif login=="Sign In":
     lg=st.form("Sign in")
