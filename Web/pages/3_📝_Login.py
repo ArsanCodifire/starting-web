d33660a7a4751d683855246d0088a371d97d2f86
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-01 21:38:17 +0530

    Update 3_📝_Login.py

--- Web/pages/3_📝_Login.py
+++ Web/pages/3_📝_Login.py
@@ -1,10 +1,19 @@
 import streamlit as st
+import supabase.com as sb
 url="https://dojvibmzlvoqnccztpvb.supabase.co"
 api="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvanZpYm16bHZvcW5jY3p0cHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjI1MjQzMzgsImV4cCI6MjAzODEwMDMzOH0.O88obpltWJCPqXrWSu_mKsgvEpU3xAREv8A9SMbjqyY"
+db=sb.create_client(url,api)
+
 def signin():
+    auth=db.auth.sign_up(email=email,password=psd)
     st.write(f"Hi{usr}")
+def logout():
+    db.auth.sign_out()
 def login():
+    session=db.auth.sign_in(email=emaill,password=psdl)
     st.write(f"Welcome back{usr}")
+    st.button("Login Out",help=None,on_click=logout)
+    
 #comment
 login=st.selectbox("Choose",options=("NA","Sign In","Login"))
 if login=="Login":
