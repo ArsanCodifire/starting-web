262686431efa7f262cbda40a4a61b25206ffa2b9
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-21 18:53:05 +0530

    Update 3_📩_Links.py

--- Web/pages/3_📩_Links.py
+++ Web/pages/3_📩_Links.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-st.page_config(page_title="Links",page_icon=":email:")
+st.set_page_config(page_title="Links",page_icon=":email:")
 st.title("Links")
 url = "https://discord.gg/MYYn3Gr8Tt"
 link_text = "Discord Server"
