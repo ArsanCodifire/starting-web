870b48748fd4edbabe16a46613357e1bbf9e88ff
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-23 16:25:08 +0530

    Update 5_💻_AI.py

--- Web/pages/5_💻_AI.py
+++ Web/pages/5_💻_AI.py
@@ -12,7 +12,7 @@ def gen(prompt):
     return response.generations[0].text.strip()
 msg=st.container(height=250)
 pr=msg.chat_input()
-with msg.chat_message("user"):
+while True:
     try:
         msg.chat_message("user").write(pr)
         msg.chat_message("assistant").write(gen(pr))
