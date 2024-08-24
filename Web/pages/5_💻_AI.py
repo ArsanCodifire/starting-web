03b4edb9a1354803f28f880bfa9908d5463503b7
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-08-24 17:50:02 +0530

    Update 5_💻_AI.py

--- Web/pages/5_💻_AI.py
+++ Web/pages/5_💻_AI.py
@@ -1,18 +1,21 @@
 import streamlit as st
 import cohere as ai
+
 token=st.secrets["TOKEN"]
-def gen(prompt):
+def gen(prompt,mdl):
     co = ai.Client(token)
-    mdl="command-xlarge-nightly"
     response = co.generate(
         model=mdl,
         prompt=prompt,
         max_tokens=1000 
     )
     return response.generations[0].text.strip()
+
+mdl=st.selectbox("Models",["command-xlarge-nightly","command-medium-nightly","generate-code-nightly"]) 
+
 msg=st.container(height=250)
 pr=msg.chat_input()
 with msg.chat_message("user"):
     msg.chat_message("user").write(pr)
-    msg.chat_message("assistant").write(gen(pr))
+    msg.chat_message("assistant").write(gen(pr,mdl))
 
