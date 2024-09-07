5e2a8fc446f1ab08a30ec9b1ac59db8099392e9f
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-09-07 23:29:53 +0530

    Update 4_📊_DiscordStats.py

--- Web/pages/4_📊_DiscordStats.py
+++ Web/pages/4_📊_DiscordStats.py
@@ -3,8 +3,8 @@ import time
 import streamlit.components.v1 as prt
 st.set_page_config(page_title="Discord Stats",page_icon=":robot:")
 st.title("Discord Pybot is Running")
-btn=st.button("Rerun")
 def reload():
   prt.iframe("https://discord.com/widget?id=1264247217158623262&theme=dark", width=350,height=500)
+btn=st.button("Rerun")
 if btn:
   reload()
