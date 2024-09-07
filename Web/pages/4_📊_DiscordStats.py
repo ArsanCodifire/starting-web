b7b1c822dedfcbd298d04a7db926868456f68eb9
ArsanCodifire <132670256+ArsanCodifire@users.noreply.github.com>
2024-09-07 22:59:56 +0530

    Update 4_📊_DiscordStats.py

--- Web/pages/4_📊_DiscordStats.py
+++ Web/pages/4_📊_DiscordStats.py
@@ -2,4 +2,5 @@ import streamlit as st
 import streamlit.components.v1 as prt
 st.set_page_config(page_title="Discord Stats",page_icon=":robot:")
 st.title("Discord Pybot is Running")
-prt.iframe("https://discord.com/widget?id=1264247217158623262&theme=dark", width=350,height=500)
+while st.rereun():
+  prt.iframe("https://discord.com/widget?id=1264247217158623262&theme=dark", width=350,height=500)
